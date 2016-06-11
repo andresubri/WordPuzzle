47fff2c583a1cf0e08e985a45703ae506f20406b
Carlos Andres Guillen Ubri <andriusic@gmail.com>
2016-06-11 18:39:43 -0400

    Initial commit

---
+++ SopaDeLetras/MainWindow.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace SopaDeLetras
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dificultyCmb = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.anagramTxt = new System.Windows.Forms.TextBox();
+            this.fileTxt = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.ColumnHeadersVisible = false;
+            this.dgv.Location = new System.Drawing.Point(174, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.RowHeadersWidth = 10;
+            this.dgv.Size = new System.Drawing.Size(1164, 688);
+            this.dgv.TabIndex = 0;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 648);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Crear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(93, 648);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 2;
+            this.saveBtn.Text = "Guardar";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 677);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cargar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 677);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Resolver";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dificultyCmb
+            // 
+            this.dificultyCmb.FormattingEnabled = true;
+            this.dificultyCmb.Items.AddRange(new object[] {
+            "Facil",
+            "Medio",
+            "Dificil(Equipos potentes)",
+            "God(Don\'t try this at home)"});
+            this.dificultyCmb.Location = new System.Drawing.Point(6, 19);
+            this.dificultyCmb.Name = "dificultyCmb";
+            this.dificultyCmb.Size = new System.Drawing.Size(141, 21);
+            this.dificultyCmb.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dificultyCmb);
+            this.groupBox1.Location = new System.Drawing.Point(15, 590);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(153, 52);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dificultad";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.anagramTxt);
+            this.groupBox2.Location = new System.Drawing.Point(12, 343);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(156, 241);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Anagramas";
+            // 
+            // anagramTxt
+            // 
+            this.anagramTxt.Location = new System.Drawing.Point(9, 20);
+            this.anagramTxt.Multiline = true;
+            this.anagramTxt.Name = "anagramTxt";
+            this.anagramTxt.Size = new System.Drawing.Size(141, 215);
+            this.anagramTxt.TabIndex = 0;
+            // 
+            // fileTxt
+            // 
+            this.fileTxt.Location = new System.Drawing.Point(9, 16);
+            this.fileTxt.Multiline = true;
+            this.fileTxt.Name = "fileTxt";
+            this.fileTxt.Size = new System.Drawing.Size(141, 302);
+            this.fileTxt.TabIndex = 5;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.fileTxt);
+            this.groupBox3.Location = new System.Drawing.Point(12, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(156, 324);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Palabras";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1350, 712);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgv);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox dificultyCmb;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox anagramTxt;
+        private System.Windows.Forms.TextBox fileTxt;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ SopaDeLetras/MainWindow.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using SopaDeLetras.Properties;
+
+// ReSharper disable InvertIf
+
+namespace SopaDeLetras
+{
+    public partial class MainWindow : Form
+    {
+        private const string Filename = @"sopa.csv";
+        private const string AnagFile = @"anagramas.txt";
+        private const string Path = @"C:\Users\andriusic\Documents\Visual Studio 2015\Projects\SopaDeLetras\SopaDeLetras\";
+        private readonly string[] _words = File.ReadAllLines(Path + "palabras.txt");
+        private readonly Random _rnd = new Random();
+        private int _firstVal;
+        private string[] _bothLists;
+        public MainWindow()
+        {
+            InitializeComponent();
+            fileTxt.Lines = _words;
+            dificultyCmb.SelectedIndex = 0;
+            LoadAnagrams();
+        }
+
+        public void ConcatenateFiles()
+        {
+            _bothLists = new string[fileTxt.Lines.Length + anagramTxt.Lines.Length];
+            fileTxt.Lines.CopyTo(_bothLists, 0);
+            anagramTxt.Lines.CopyTo(_bothLists, fileTxt.Lines.Length);
+        }
+
+        public void LoadAnagrams()
+        {
+            var anagramList = File.ReadAllLines(Path + AnagFile);
+            var foundAnagrams = WordFingerprint(fileTxt.Lines, anagramList);
+            anagramTxt.Lines = foundAnagrams;
+            ConcatenateFiles();
+        }
+
+        public string[] WordFingerprint(string[] wordsList, string[] anagramsList)
+        {
+            List<string> result = new List<string>();
+            
+
+            for (var a = 0; a < anagramsList.Length; a++)
+            {
+                for (var i = 0; i < wordsList.Length; ++i)
+                {
+                    if (anagramsList.ElementAt(a).Length == wordsList.ElementAt(i).Length)
+                    {
+                        int anagramsFingerprint = 0;
+                        int wordListFingerprint = 0;
+
+                        for (int j = 0; j < anagramsList.ElementAt(a).Length; j++)
+                        {
+                            anagramsFingerprint = anagramsFingerprint + anagramsList.ElementAt(a).ElementAt(j);
+                            wordListFingerprint = wordListFingerprint + wordsList.ElementAt(i).ElementAt(j);
+                            if (anagramsFingerprint == wordListFingerprint && j == anagramsList.ElementAt(a).Length-1)
+                            {
+                                if (!result.Contains(anagramsList.ElementAt(a)))
+                                {
+                                    result.Add(anagramsList.ElementAt(a));
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+                }
+            return result.ToArray();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConcatenateFiles();
+
+            switch (dificultyCmb.SelectedIndex)
+                {
+                    case 0:
+                        CreatePuzzle(_bothLists, 4);
+                        break;
+                    case 1:
+                        CreatePuzzle(_bothLists, 3);
+                        break;
+                    case 2:
+                        CreatePuzzle(_bothLists, 2);
+                        break;
+                    case 3:
+                        CreatePuzzle(_bothLists, 1);
+                        break;
+                    default:
+                        MessageBox.Show(Resources.MainWindow_button1_Click_Favor_seleccionar_dificultad);
+                        break; 
+            }
+        }
+
+        public bool RowValidator(int posX ,string word, int dgvSize)
+        {
+            return posX + word.Length < dgvSize;
+        }
+
+        public bool ColumnValidator(int posY, string word, int dgvSize)
+        {
+            return word.Length + posY < dgvSize;
+        }
+
+        //Verify that the assigned spaces are null for the horizontal word
+        public bool RowContentCheck(int posX, int posY, string word)
+        {
+            var aux = 0;
+            for (var o = 0; o <= word.Length; o++)
+            {
+                if (dgv[posX, posY].Value != null)
+                {
+                    aux++;
+                }
+                posX++;
+            }
+            return aux <= 0;
+        }
+        
+        //Verify that the assigned spaces are null for the vertical word
+        public bool ColumnContentCheck(int posX, int posY, string word)
+        {
+            var aux = 0;
+            for (var o = 0; o <= word.Length; o++)
+            {
+                if (dgv[posX, posY].Value != null)
+                {
+                    aux++;
+                }
+                posY++;
+            }
+            return aux <= 0;
+        }
+
+        public void CreatePuzzle(string[] wordList, int dificulty)
+        {
+            //Load file
+            button1.Enabled = false;
+            //Clear DatagridView 
+            ClearDgv(dgv);
+            var wordListSize = 0;
+            //Define DatagridView Dimentions
+            for (int i = 0; i < wordList.Length; i++)
+            {
+                for (int a = 0; a < wordList.ElementAt(i).Length; a++)
+                {
+                    wordListSize++;
+                }
+                
+            }
+            if (wordListSize < 50)
+            {
+                dgv.RowCount = wordListSize / 2;
+                dgv.ColumnCount = wordListSize / 2;
+
+            }
+            else
+            {
+                dgv.RowCount = wordListSize / dificulty;
+                dgv.ColumnCount = wordListSize / dificulty;
+            }
+
+            //Creates rows and colums
+            for (var i = 0; i < wordList.Length; i++)
+            {
+                    var word = wordList.ElementAt(i);
+                    var posX = _rnd.Next(1,dgv.RowCount);
+                    var posY = _rnd.Next(1, dgv.ColumnCount);
+                    
+                if (i % 2 == 0)
+                {
+                    //Creates horizontal
+                        if (dgv[posX, posY].Value == null  && RowValidator(posX, word,dgv.RowCount) && RowContentCheck(posX,posY,word))
+                        {
+                            for (var a = 0; a < word.Length; a++)
+			                {
+                            if(dgv[posX, posY].Value == null)
+                            {
+                                dgv[posX, posY].Value = word.ElementAt(a);
+                                posX++;
+                            }
+			                
+                            }
+                        }else{
+                            i--;
+                        }
+                 }
+                else
+                {
+                    //Creates vertical
+                    if (dgv[posX, posY].Value == null && ColumnValidator(posY, word, dgv.ColumnCount) && ColumnContentCheck(posX, posY, word))
+                    {
+                        for (var a = 0; a < word.Length; a++)
+                        {
+
+                            if (dgv[posX, posY].Value == null)
+                            {
+                                dgv[posX, posY].Value = word.ElementAt(a);
+                                posY++;
+                            }
+                            else
+                            {
+                                for (var e = 0; e < a; e++)
+                                {
+                                    posX = _rnd.Next(1, dgv.RowCount);
+                                    posY = _rnd.Next(1, dgv.ColumnCount);
+                                    dgv[posX, posY].Value = null;
+                                    i--;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+            }
+            FillDgv(dgv);
+            button1.Enabled = true;
+        }
+
+        //Fill blank spaces with random letters
+        public void FillDgv(DataGridView dataGrid)
+        {
+            var randomChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (var i = 0; i < dataGrid.ColumnCount; i++)
+            {
+                for (var u = 0; u < dataGrid.RowCount; u++)
+                {
+                    if (dataGrid[u, i].Value == null)
+                    {
+                        //Picks a random char from the string above
+                        dataGrid[u, i].Value = randomChar.ElementAt(_rnd.Next(1, randomChar.Length));
+                    }
+                }
+            }
+        }
+
+        public void SolveHorizontalPuzzle(string[] dict)
+        {
+            //For runs through the dictionary    
+            for (var i = 0; i < dict.Length; i++)
+            {
+                var currentWord = dict.ElementAt(i);
+                //For runs through each row
+                for (var row = 0; row < dgv.RowCount; row++)
+                {
+                    //For runs through each column
+                    for (int column = 0; column < dgv.ColumnCount; column++)
+                    {
+                        string value = dgv.Rows[row].Cells[column].Value.ToString();
+                        if (value.ElementAt(0) == currentWord.ElementAt(0) && currentWord.Length + column < dgv.ColumnCount)
+                        {
+                            var completeWord = "";
+                            //Calculate fingerprint
+                            //For runs through word to compare 
+                            for (var index = 0; index < currentWord.Length; index++)
+                            {
+                                if (index == 0)
+                                {
+                                    _firstVal = index + column;
+                                }
+                                completeWord = completeWord + dgv.Rows[row].Cells[index + column].Value;
+                                var fingerPrintCurrent = ValidateWord(currentWord, completeWord);
+                                if (fingerPrintCurrent)
+                                {
+                                    for (int s = index + column; s >= _firstVal; s--)
+                                    {
+                                        dgv[s, row].Selected = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void SolveVerticalPuzzle(string[] dict)
+        {
+            //For runs through the dictionary    
+            for (int i = 0; i < dict.Length; i++)
+            {
+                var currentWord = dict.ElementAt(i);
+                //For runs through each row
+                for (int row = 0; row < dgv.ColumnCount; row++)
+                {
+                    //For runs through each column
+                    for (int column = 0; column < dgv.RowCount; column++)
+                    {
+                        string value = dgv.Rows[row].Cells[column].Value.ToString();
+                        if (value.ElementAt(0) == currentWord.ElementAt(0) && currentWord.Length + row < dgv.RowCount)
+                        {
+                            string completeWord = "";
+                            _firstVal = 0;
+                            //For runs through word to compare 
+                            for (int index = 0; index < currentWord.Length+1; index++)
+                            {
+                                if (index == 0)
+                                {
+                                    _firstVal = row;
+                                }
+                                completeWord = completeWord + dgv.Rows[index+row].Cells[column].Value;
+                                bool fingerPrintCurrent = ValidateWord(currentWord, completeWord);
+                                if (fingerPrintCurrent)
+                                {
+                                    for (int s = index + row; s >= _firstVal; s--)
+                                    {
+                                        dgv[column, s].Selected = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public bool ValidateWord(string word, string wordCompare)
+        {
+            int aux = 0;
+            for (int i = 0; i < wordCompare.Length; i++)
+            {
+                if (word.Length == wordCompare.Length)
+                {
+                    if (word.ElementAt(i) == wordCompare.ElementAt(i))
+                    {
+                        aux++;
+                    }
+                }
+            }
+            return aux == word.Length;
+        }
+
+        public void ClearDgv(DataGridView dataGrid)
+        {
+            if(dataGrid.DataSource != null)
+            {
+                dataGrid.DataSource = null;
+            }
+            dataGrid.Rows.Clear();
+            dataGrid.Refresh();
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            //Saves the content in DataGridView to a .csv file
+            dgv.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithAutoHeaderText;
+            dgv.SelectAll();
+            //Copy selection to clipboard
+            // ReSharper disable once AssignNullToNotNullAttribute
+            Clipboard.SetDataObject(dgv.GetClipboardContent());
+            //Get the clipboard and serialize it to the file
+            File.WriteAllText(Path + Filename, Clipboard.GetText(TextDataFormat.CommaSeparatedValue));
+            dgv.ClearSelection();
+        }
+        
+        //Loads the file to the DataGridView
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearDgv(dgv);
+            //Setup the connection to the path of the file
+            var conn = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0; Data Source = " +
+                Path + "; Extended Properties = \"Text;HDR=NO;FMT=Delimited\"");
+            conn.Open();
+            //Setup the adapter with the query
+            var adapter = new OleDbDataAdapter
+                   ("SELECT * FROM " + "sopa.csv", conn);
+            var ds = new DataTable();
+            //Fill the DataTable with the adapter
+            adapter.Fill(ds);
+            conn.Close();
+            dgv.RowCount = 0;
+            dgv.ColumnCount = 0;
+            dgv.DataSource = ds;
+            dgv.Refresh();
+        }
+
+
+        public void ShowResult(DataGridView dataGrid)
+        {
+            for (var i = 0; i < dataGrid.ColumnCount; i++)
+            {
+                for (var u = 0; u < dataGrid.RowCount; u++)
+                {
+                    if (!dataGrid[u, i].Selected)
+                    {
+                        dataGrid[u, i].Value = " ";
+                    }
+                }
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ConcatenateFiles();
+            SolveHorizontalPuzzle(_bothLists);
+            SolveVerticalPuzzle(_bothLists);
+            dgv[0, 0].Selected = false;
+            ShowResult(dgv);
+            dgv.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithAutoHeaderText;
+            //Copy selection to clipboard
+            dgv.SelectAll();
+            // ReSharper disable once AssignNullToNotNullAttribute
+            Clipboard.SetDataObject(dgv.GetClipboardContent());
+            //Get the clipboard and serialize it to the file
+            File.WriteAllText(Path + "solucion.txt", Clipboard.GetText(TextDataFormat.CommaSeparatedValue));
+            dgv.ClearSelection();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
